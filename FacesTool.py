89feb326cb55be3a60c0229cb1f573e92f770667
aschwartz91 <77259180+aschwartz91@users.noreply.github.com>
2022-08-17 23:56:12 -0400

    TEST

--- FacesTool.py
+++ FacesTool.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
+import time#add
 
 options = Options()
 options.headless = True
